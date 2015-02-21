946de0a431b2dd556f085bf3f5cf151c1ab98d15
Pavel Senin <seninp@gmail.com>
2015-02-21 22:28:38 +0100

    sharing results

--- res/processing.R
+++ res/processing.R
@@ -1,12 +1,18 @@
 require(stringr)
 require(plyr)
 
-data = read.csv("../res/errors.txt",header=F,sep=" ",as.is=T)
-str(data)
+datasets=read.csv("../res/datasets.txt",header=F,sep=" ",as.is=T)
+names(datasets)=c("dataset","classes","train_size","test_size","length","error_euclidean","error_dtw")
 
-saxvsm=data[data$V1=="SAXVSMContinuousDirectSampler",]
-saxvsmg=data[data$V1=="SAXVSMContinuousGrammarSampler",]
+tmp = read.csv("../res/errors_sax_vsm.txt",header=F,sep=" ",as.is=T)
+names(tmp)=c("dataset","classic","exact","nored")
+sax_vsm = data.frame(dataset=tmp$dataset, error=apply(tmp[,2:4],1,min))
 
-saxvsm = saxvsm[ order(saxvsm$V2), -1]
+tmp2 = read.csv("../res/errors_sax_vsm-g.txt",header=F,sep=" ",as.is=T)
+names(tmp2)=c("dataset","classic","exact","nored")
+sax_vsm_g = data.frame(dataset=tmp2$dataset, error=apply(tmp2[,2:4],1,min))
 
-write.table(saxvsm,"../res/errors_sax_vsm.txt",row.names=F,col.names=F)
+data=merge(datasets,sax_vsm,by.x=c("dataset"),by.y=c("dataset"),all=T)
+data=merge(data,sax_vsm_g,by.x=c("dataset"),by.y=c("dataset"),all=T)
+
+write.table(data,"../res/summary.txt",row.names=F,col.names=T)
