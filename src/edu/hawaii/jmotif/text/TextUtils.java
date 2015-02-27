d844c8f2791009cfafa7b5e0e8a9ada27708065b
Pavel Senin <seninp@gmail.com>
2015-02-27 15:31:21 +0100

    minor fix

--- src/edu/hawaii/jmotif/text/TextUtils.java
+++ src/edu/hawaii/jmotif/text/TextUtils.java
@@ -108,13 +108,13 @@ public class TextUtils {
           double tfValue = 1.0D + Math.log(Integer.valueOf(wordInBagFrequency).doubleValue());
 
           // OSULeaf: 0.1405
-          // double tfValue = normalizedTF(bag, word.getKey());
+//           double tfValue = normalizedTF(bag, word.getKey());
 
           // OSULeaf: 0.08678
-          // double tfValue = augmentedTF(bag, word.getKey());
+//           double tfValue = augmentedTF(bag, word.getKey());
 
           // OSULeaf: 0.08678
-          // double tfValue = logAveTF(bag, word.getKey());
+//           double tfValue = logAveTF(bag, word.getKey());
 
           // compute the IDF
           //
